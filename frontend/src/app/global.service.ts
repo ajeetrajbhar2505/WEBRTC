af3deb28ef26e5735bc51c12777c9748062271cc
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-02-17 23:56:55 +0530

    updated frontend

--- frontend/src/app/global.service.ts
+++ frontend/src/app/global.service.ts
@@ -1,23 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { environment } from '../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalService {
   notificationSubject:Subject<any> = new Subject<any>()
-
   constructor(private http:HttpClient) {
   }
 
   getData<T>(endpoint: string): Observable<T> {
-    const url = `${this.baseUrl}/${endpoint}`;
+    const url = `${environment.baseUrl}/${endpoint}`;
     return this.http.get<T>(url);
   }
 
   postData<T>(endpoint: string, data: any): Observable<T> {
-    const url = `${this.baseUrl}/${endpoint}`;
+    const url = `${environment.baseUrl}/${endpoint}`;
     return this.http.post<T>(url, data);
   }
 }
